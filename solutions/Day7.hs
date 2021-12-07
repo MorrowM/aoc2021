25adf03c91339d060ff31e507c43e0a6024c862d
MorrowM <themorrowm@gmail.com>
2021-12-07 16:35:14 +0200

    More pointfree

--- solutions/Day7.hs
+++ solutions/Day7.hs
@@ -4,4 +4,4 @@ main :: IO ()
 main = parseAndRun 7 (pInt `sepBy` pComma) (moveWith id) (moveWith $ \n -> n * (n + 1) `quot` 2)
 
 moveWith :: (Int -> Int) -> [Int] -> Int
-moveWith f locs = minimum [sum . map (\loc -> f . abs $ loc - goto) $ locs | goto <- [minimum locs .. maximum locs]]
+moveWith f locs = minimum [sum . map (f . abs . subtract goto) $ locs | goto <- [minimum locs .. maximum locs]]
